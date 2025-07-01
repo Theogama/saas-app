80d771f1f6c5402fb2b5ab0ab6aff75128d62e52
Theogama <sandilegama.sg@gmail.com>
2025-07-02 00:18:35 +0200

    implemented getCompanion, SessionsHistory, recentSessios and sentry set up

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://cc89567b67d2b84d9f7dc9c346cee5c6@o4507324424519680.ingest.us.sentry.io/4507593388064768",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
